9cbcc8c7462a37bfd24720295ab34b6d58ee6264
Mattia Louis Vik Bradascio <mattia@Mattias-MacBook-Pro.local>
2018-12-08 11:55:59 +0100

    added wuclient context and subscriber

---
+++ src/wuclient.cpp
@@ -0,0 +1,15 @@
+#include <zmq.hpp>
+#include <iostream>
+#include <sstream>
+
+int main (int argc, char *argv[])
+{
+  zmq::context_t context(1);
+
+  // Socket should talk to the server
+  std::cout <<"Collecting updates from weather server... \n" << std::endl;
+  zmq::socket_t subscriber(context, ZMQ_SUB);
+  subscriber.connect("tcp://localhost::556");
+
+  
+}
