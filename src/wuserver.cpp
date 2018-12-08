a2bc70dff89a878d9dd975be1971a8346edd45a6
Mattia Louis Vik Bradascio <mattia@Mattias-MacBook-Pro.local>
2018-12-08 11:53:27 +0100

    added binary weather update server

--- src/wuserver.cpp
+++ src/wuserver.cpp
@@ -3,8 +3,36 @@
 #include <stdlib.h>
 #include <time.h>
 
-#if (defined (WIN32))
+#if(defined (WIN32))
 #include <zhelpers.hpp>
 #endif
 
-#define within(num) (int) ((float) num * random() /(RAND_MAX + 1.0))
+#define within(num) (int) ((float) num * random() / (RAND_MAX + 1.0))
+
+int main(){
+
+  // Prepare context and publisher
+  zmq::context_t context(1);
+  zmq::socket_t publisher(context, ZMQ_PUB);
+  publisher.bind("tcp://*:5556");
+  publisher.bind("ipc://weather.ipc");
+
+  // Random number generator
+  srandom((unsigned) time(NULL));
+  while(1){
+    int zipcode, temperature, relhumidity;
+
+    // Get the values
+    zipcode = within(100000);
+    temperature = within(215)-80;
+    relhumidity = within(50) + 10;
+
+    // Send messages to all subscribers
+    zmq::message_t message(20);
+    snprintf( (char *) message.data(), 20,
+      "%05d %d %d", zipcode, temperature, relhumidity);
+    publisher.send(message);
+  }
+
+  return 0;
+}
