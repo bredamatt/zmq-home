ab57d70bfaf18400c07de4a11b6bedba8f142107
Mattia Louis Vik Bradascio <mattia@Mattias-MacBook-Pro.local>
2018-12-08 13:44:09 +0100

    Finish taskwork.cpp

--- src/taskwork.cpp
+++ src/taskwork.cpp
@@ -15,11 +15,35 @@ int main (int argc, char *argv[])
     context_t context(1);
 
     // Receive messages on this socket:
-    socket_t receiver(context, ZMQ_PULL);
+    socket_t receiver(context, ZMQ_PULL); // define receive with ZMQ_PULL
     receiver.connect("tcp://localhost:5557");
 
     // Socket to send messages to
-    socket_t sender(context, ZMQ_PUSH);
+    socket_t sender(context, ZMQ_PUSH); // define send with ZMQ_PUSH
     sender.connect("tcp://localhost:5558");
-    
+
+    // Should always process tasks
+    while (1) {
+
+      // Serialize the message
+      message_t message;
+      int workload;
+      receiver.recv(&message);
+
+      // Deserialize using string and istringstream
+      string smessage(static_cast<char*>(message.data()), message.size());
+      istringstream iss(smessage);
+      iss >> workload;
+
+      // Do the work
+      s_sleep(workload);
+
+      // Send results to sink
+      message.rebuild();
+      sender.send(message);
+
+      cout << "." << flush;
+    }
+    return 0;
+
 }
