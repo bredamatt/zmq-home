826c90ec2bd5f50a362d83d010c55ef5c21bc3bb
Mattia Louis Vik Bradascio <mattia@Mattias-MacBook-Pro.local>
2018-12-25 20:24:04 +0100

    Created context and PULL request socket

--- src/Parallel-pipeline/taskworker2.cpp
+++ src/Parallel-pipeline/taskworker2.cpp
@@ -1,2 +1,18 @@
 // Task worker (round 2) in C++
 // Due to difficulties with terminating the process when running taskworker
+
+#include "zhelpers.cpp"
+#include <string>
+
+int main (int argc, char *argv[])
+{
+
+  // Create the context
+  zmq::context_t context(1);
+
+  // Create the receive socket, or pull req
+  zmq::socket_t receiver(context, ZMQ_PULL);
+  receiver.connect("tcp://localhost:5557");
+
+  // Create the send socket. or push req 
+}
